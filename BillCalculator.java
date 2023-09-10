9b729da2b0c56cac6ed649af56341ef360e48554
Veena161 <85404354+Veena161@users.noreply.github.com>
2023-09-11 01:19:04 +0530

    Add files via upload

--- BillCalculator.java
+++ BillCalculator.java
@@ -1,10 +1,23 @@
-package electricitybillsystem;
+package com.mphasis.billsystem;
 
 public class BillCalculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int units = 280;
+		double billTopay = 0;
+		if(units < 100)
+		{		
+			billTopay = units * 1.20;
+		}
+		else if(units < 300)
+		{
+			billTopay = 100 * 1.20 + 
+					(units - 100) * 2;
+			
+		}
 		
+		System.out.println("The electricity bill for" +units+ " is : " + billTopay);
 
 	}
 
